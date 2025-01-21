8f4adb64f2b96607ba197d32e6af5b1a69e69e54
Umair Khan <125242896+mrumairkhan74@users.noreply.github.com>
2025-01-21 10:23:05 +0500

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../App.css'
+const About = () => {
+  return (
+    <>
+    <div className="container">
+        <h1 className="section-h1">About</h1>
+    </div>
+    <div className="about">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolore sequi omnis eligendi possimus vitae magnam quae provident, nesciunt inventore iure temporibus tenetur maiores ipsum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quaerat ipsam unde est laborum asperiores perspiciatis, vel, labore, amet excepturi dicta quam debitis earum aliquam.</p>
+        <button className='contact-btn-2'>Read More</button>
+    </div>
+    </>
+  )
+}
+
+export default About
